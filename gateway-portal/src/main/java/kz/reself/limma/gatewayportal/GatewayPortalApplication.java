d7c696a78cc40747a736eece4ab0789f5728a73f
nurdauletalim <nurdauletalim20@gmail.com>
2021-12-10 10:31:10 +0600

    Let's goo

---
+++ gateway-portal/src/main/java/kz/reself/limma/gatewayportal/GatewayPortalApplication.java
@@ -0,0 +1,13 @@
+package kz.reself.limma.gatewayportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayPortalApplication.class, args);
+	}
+
+}
---
+++ gateway-portal/src/test/java/kz/reself/limma/gatewayportal/GatewayPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.reself.limma.gatewayportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
