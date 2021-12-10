699fc1da54ae8516e3f8abcd85147b784e9ba77e
nurdauletalim <nurdauletalim20@gmail.com>
2021-12-10 15:49:51 +0600

    config

--- gateway-crm/src/main/java/kz/reself/limma/gatewaycrm/GatewayCrmApplication.java
+++ gateway-crm/src/main/java/kz/reself/limma/gatewaycrm/GatewayCrmApplication.java
@@ -2,8 +2,10 @@ package kz.reself.limma.gatewaycrm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class GatewayCrmApplication {
 
 	public static void main(String[] args) {
--- gateway-portal/src/main/java/kz/reself/limma/gatewayportal/GatewayPortalApplication.java
+++ gateway-portal/src/main/java/kz/reself/limma/gatewayportal/GatewayPortalApplication.java
@@ -2,8 +2,10 @@ package kz.reself.limma.gatewayportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class GatewayPortalApplication {
 
 	public static void main(String[] args) {
