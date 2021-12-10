9ee70f75eab3db541eb0680f602af2c135edd1e6
nurdauletalim <nurdauletalim20@gmail.com>
2021-12-10 12:19:31 +0600

    config

--- promotion/src/main/java/kz/reself/limma/promotion/PromotionApplication.java
+++ promotion/src/main/java/kz/reself/limma/promotion/PromotionApplication.java
@@ -2,8 +2,10 @@ package kz.reself.limma.promotion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PromotionApplication {
 
 	public static void main(String[] args) {
