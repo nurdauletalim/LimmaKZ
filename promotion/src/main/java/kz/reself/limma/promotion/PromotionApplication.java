70b1d02c3631e0982b35debf11a22fbde788afab
progaza <fakhrudinov.azamat@gmail.com>
2021-12-21 11:10:00 +0600

    add hystrix

--- promotion/src/main/java/kz/reself/limma/promotion/PromotionApplication.java
+++ promotion/src/main/java/kz/reself/limma/promotion/PromotionApplication.java
@@ -3,9 +3,13 @@ package kz.reself.limma.promotion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
+@EnableHystrixDashboard
 public class PromotionApplication {
 
 	public static void main(String[] args) {
--- promotion/src/main/java/kz/reself/limma/promotion/service/impl/PromotionImageService.java
+++ promotion/src/main/java/kz/reself/limma/promotion/service/impl/PromotionImageService.java
@@ -1,5 +1,7 @@
 package kz.reself.limma.promotion.service.impl;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import kz.reself.limma.promotion.model.PromotionImage;
 import kz.reself.limma.promotion.repository.PromotionImageRepository;
 import kz.reself.limma.promotion.service.IPromotionImageService;
@@ -17,36 +19,85 @@ public class PromotionImageService implements IPromotionImageService {
     private PromotionImageRepository promotionImageRepository;
 
     @Override
+    @HystrixCommand(
+            fallbackMethod = "findAllImagesPageable",
+            threadPoolKey = "alternativeMethod",
+            threadPoolProperties = {
+                    @HystrixProperty(name = "coreSize", value = "100"),
+                    @HystrixProperty(name = "maxQueuesize", value = "50"),
+            })
     public Page<PromotionImage> findAllImagesPageable(Pageable pageable) {
         return this.promotionImageRepository.findAll(pageable);
     }
 
     @Override
+    @HystrixCommand(
+            fallbackMethod = "findAllImagesIterable",
+            threadPoolKey = "alternativeMethod",
+            threadPoolProperties = {
+                    @HystrixProperty(name = "coreSize", value = "100"),
+                    @HystrixProperty(name = "maxQueuesize", value = "50"),
+            })
     public List<PromotionImage> findAllImagesIterable() {
         return this.promotionImageRepository.findAll();
     }
 
     @Override
+    @HystrixCommand(
+            fallbackMethod = "getAllImageByPromotionId",
+            threadPoolKey = "alternativeMethod",
+            threadPoolProperties = {
+                    @HystrixProperty(name = "coreSize", value = "100"),
+                    @HystrixProperty(name = "maxQueuesize", value = "50"),
+            })
     public List<PromotionImage> getAllImageByPromotionId(Integer promotionId) {
         return this.promotionImageRepository.findAllByPromotionId(promotionId);
     }
 
     @Override
+    @HystrixCommand(
+            fallbackMethod = "findImageById",
+            threadPoolKey = "alternativeMethod",
+            threadPoolProperties = {
+                    @HystrixProperty(name = "coreSize", value = "100"),
+                    @HystrixProperty(name = "maxQueuesize", value = "50"),
+            })
     public PromotionImage findImageById(Integer id) {
         return this.promotionImageRepository.getById(id);
     }
 
     @Override
+    @HystrixCommand(
+            fallbackMethod = "createImage",
+            threadPoolKey = "alternativeMethod",
+            threadPoolProperties = {
+                    @HystrixProperty(name = "coreSize", value = "100"),
+                    @HystrixProperty(name = "maxQueuesize", value = "50"),
+            })
     public PromotionImage createImage(PromotionImage image) {
         return this.promotionImageRepository.save(image);
     }
 
     @Override
+    @HystrixCommand(
+            fallbackMethod = "updateImage",
+            threadPoolKey = "alternativeMethod",
+            threadPoolProperties = {
+                    @HystrixProperty(name = "coreSize", value = "100"),
+                    @HystrixProperty(name = "maxQueuesize", value = "50"),
+            })
     public List<PromotionImage> updateImage(List<PromotionImage> image) {
         return this.promotionImageRepository.saveAll(image);
     }
 
     @Override
+    @HystrixCommand(
+            fallbackMethod = "deleteImageById",
+            threadPoolKey = "alternativeMethod",
+            threadPoolProperties = {
+                    @HystrixProperty(name = "coreSize", value = "100"),
+                    @HystrixProperty(name = "maxQueuesize", value = "50"),
+            })
     public void deleteImageById(Integer id) {
         PromotionImage image = this.promotionImageRepository.getOne(id);
         this.promotionImageRepository.delete(image);
@@ -54,6 +105,13 @@ public class PromotionImageService implements IPromotionImageService {
     }
 
     @Override
+    @HystrixCommand(
+            fallbackMethod = "getImagesById",
+            threadPoolKey = "alternativeMethod",
+            threadPoolProperties = {
+                    @HystrixProperty(name = "coreSize", value = "100"),
+                    @HystrixProperty(name = "maxQueuesize", value = "50"),
+            })
     public List<RImages> getImagesById(Integer promotionId) {
         List<RImages> rImagesList = new ArrayList<>();
         List<PromotionImage> images = getAllImageByPromotionId(promotionId);
@@ -67,5 +125,9 @@ public class PromotionImageService implements IPromotionImageService {
             ));
         }
         return rImagesList;
+    }
+
+    public String alternativeMethod(){
+        return "Something get wrong";
     }
 }
